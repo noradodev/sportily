2e2a9feab3c5c56af1f19e8ae1c7bf6f1dde47a0
Kaneka0-0 <kaneka.doung12@gmail.com>
2024-03-29 22:24:29 +0700

    up_del

--- app/src/main/java/com/rado/spotilyapp/ui/activities/up_del.java
+++ app/src/main/java/com/rado/spotilyapp/ui/activities/up_del.java
@@ -6,12 +6,11 @@ import android.os.Bundle;
 
 import com.rado.spotilyapp.R;
 
-public class update_del extends AppCompatActivity {
+public class up_del extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        setContentView(R.layout.activity_up_del);
     }
 }
